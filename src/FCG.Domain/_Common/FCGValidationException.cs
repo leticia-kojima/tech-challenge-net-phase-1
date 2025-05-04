69a973fdb2736ea7d46c4e6a11157d7e766d30ea
PauloBusch <paulo202015@outlook.com.br>
2025-05-04 18:56:58 +0000

    feat: add domain validation exception and test cases for user creation

---
+++ src/FCG.Domain/_Common/FCGValidationException.cs
@@ -0,0 +1,10 @@
+﻿namespace FCG.Domain._Common;
+public class FCGValidationException : Exception
+{
+    public string Field { get; private set; }
+
+    public FCGValidationException(string field, string message) : base(message)
+    {
+        Field = field;
+    }
+}
